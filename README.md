33f9e967878efd3f673d09818704d0595e3aed5d
kofta999 <mostafaxxx555@gmail.com>
2023-08-29 19:09:37 +0300

    updated some files

--- README.md
+++ README.md
@@ -1,20 +1,25 @@
 # SocialApp API (WIP):
-  A project in progress made to make a social platform API similar to Twitter/X with posts, comments and likes.
+
+A project in progress made to make a social platform API similar to Twitter/X with posts, comments and likes.
 
 ## Current features:
-* Create, list, edit and delete posts.
-* Create, list, edit and delete comments on posts.
-* Like, unlike, list number of likes on a post / comment.
+
+- Create, list, edit and delete posts.
+- Create, list, edit and delete comments on posts.
+- Like, unlike, list number of likes on a post / comment.
 
 ## TODO:
+
 - [x] remove redundant words in post and comment paths ex: (/post/{create})
 - [x] clean / comment the code
 - [x] better error handling
 - [x] work on authentication
 - [ ] paginate the API
 - [ ] auth credentials validation
+- [ ] create API docs
 
 ## Tech Stack used:
+
 - Node.JS
 - MySQL
 - TypeScript
--- src/server.ts
+++ src/server.ts
@@ -5,19 +5,9 @@ import commentsRouter from "./routes/comments";
 import likesRouter from "./routes/likes";
 import authRouter from "./routes/auth";
 import relations from "./util/relations";
-import User from "./models/user";
 
 const app = express();
 app.use(express.json());
-
-// app.use((req: Request, res: Response, next) => {
-//   User.findByPk(1)
-//     .then((user) => {
-//       res.locals.user = user;
-//       next();
-//     })
-//     .catch((err) => console.log(err));
-// });
 
 app.use("/posts", postsRouter);
 app.use("/comments", commentsRouter);
@@ -27,18 +17,5 @@ relations();
 
 sequelize
   .sync()
-  .then(() => {
-    return User.findByPk(1);
-  })
-  .then((user) => {
-    if (!user) {
-      return User.create({
-        fullName: "admin",
-        email: "admin@admin.com",
-        hashedPassword: "admin",
-      });
-    }
-    return user;
-  })
   .then(() => app.listen(3000, () => console.log("Connected on port 3000")))
   .catch((err) => console.log(err));
