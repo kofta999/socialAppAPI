518d5689c8beb4d19c0b88ea18cc3e4d7df99a3a
kofta999 <mostafaxxx555@gmail.com>
2023-09-02 22:52:26 +0300

    small fix

--- src/util/db.ts
+++ src/util/db.ts
@@ -1,16 +1,7 @@
 import { Sequelize } from "sequelize";
 
 const databaseUrl =
-  process.env.DATABASE_URL ||
-  "postgres://root:gsDeE7S8ONK0GZ71awNZizcAvgWO2jDW@dpg-cjl68dtk5scs739htt4g-a.frankfurt-postgres.render.com/social_app_ep64";
-const sequelize = new Sequelize(databaseUrl, {
-  dialect: "postgres",
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false, // Set to false if using self-signed certificates
-    },
-  },
-});
+  process.env.DATABASE_URL || "mysql://root:root@localhost:3306/socialApp";
+const sequelize = new Sequelize(databaseUrl);
 
 export default sequelize;
