fbc3b52ab8800eef025c4c8587f8cafef5733682
kofta999 <mostafaxxx555@gmail.com>
2023-09-07 14:17:57 +0300

    fixed some configs

--- src/util/db.ts
+++ src/util/db.ts
@@ -1,16 +1,9 @@
 import { Sequelize } from "sequelize";
 let sequelize: Sequelize;
 
-if (process.env.NODE_ENV !== "test") {
-  const databaseUrl =
-    process.env.DATABASE_URL || "mysql://root:root@localhost:3306/socialApp";
-  sequelize = new Sequelize(databaseUrl);
-} else {
-  sequelize = new Sequelize({
-    dialect: "sqlite",
-    storage: ":memory:",
-    logging: false,
-  });
-}
+const databaseUrl =
+  process.env.DATABASE_URL || "mysql://root:root@localhost:3306/socialApp";
+  
+sequelize = new Sequelize(databaseUrl);
 
 export default sequelize;
